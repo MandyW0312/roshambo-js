7a58d2dd20429c46e120c8501142876b0420244f
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-12 13:28:03 -0500

    Added Spock and Lizard

--- public/main.js
+++ public/main.js
@@ -6,6 +6,10 @@ function main() {
   player1Paper.addEventListener('click', clickPlayer1Paper)
   const player1Scissors = document.querySelector('section.player1 i.scissors')
   player1Scissors.addEventListener('click', clickPlayer1Scissors)
+  const player1Lizard = document.querySelector('section.player1 i.lizard')
+  player1Lizard.addEventListener('click', clickPlayer1Lizard)
+  const player1Spock = document.querySelector('section.player1 i.spock')
+  player1Spock.addEventListener('click', clickPlayer1Spock)
 
   const player2Rock = document.querySelector('section.player2 i.rock')
   player2Rock.addEventListener('click', clickPlayer2Rock)
@@ -13,6 +17,10 @@ function main() {
   player2Paper.addEventListener('click', clickPlayer2Paper)
   const player2Scissors = document.querySelector('section.player2 i.scissors')
   player2Scissors.addEventListener('click', clickPlayer2Scissors)
+  const player2Lizard = document.querySelector('section.player2 i.lizard')
+  player2Lizard.addEventListener('click', clickPlayer2Lizard)
+  const player2Spock = document.querySelector('section.player2 i.spock')
+  player2Spock.addEventListener('click', clickPlayer2Spock)
 
   // Player 1 Clicks
   function clickPlayer1Rock(event) {
@@ -32,6 +40,20 @@ function main() {
   function clickPlayer1Scissors(event) {
     const player1Choice = document.querySelector('section.player1 h3')
     player1Choice.textContent = 'scissors'
+    const chosen = document.querySelector('section.player1 h4')
+    chosen.textContent = 'Has Chosen'
+  }
+
+  function clickPlayer1Lizard(event) {
+    const player1Choice = document.querySelector('section.player1 h3')
+    player1Choice.textContent = 'lizard'
+    const chosen = document.querySelector('section.player1 h4')
+    chosen.textContent = 'Has Chosen'
+  }
+
+  function clickPlayer1Spock(event) {
+    const player1Choice = document.querySelector('section.player1 h3')
+    player1Choice.textContent = 'spock'
     const chosen = document.querySelector('section.player1 h4')
     chosen.textContent = 'Has Chosen'
   }
@@ -58,6 +80,20 @@ function main() {
     chosen.textContent = 'Has Chosen'
   }
 
+  function clickPlayer2Lizard(event) {
+    const player2Choice = document.querySelector('section.player2 h3')
+    player2Choice.textContent = 'lizard'
+    const chosen = document.querySelector('section.player2 h4')
+    chosen.textContent = 'Has Chosen'
+  }
+
+  function clickPlayer2Spock(event) {
+    const player2Choice = document.querySelector('section.player2 h3')
+    player2Choice.textContent = 'spock'
+    const chosen = document.querySelector('section.player2 h4')
+    chosen.textContent = 'Has Chosen'
+  }
+
   // Game Play
   let player1Wins = 0
   let player2Wins = 0
@@ -72,19 +108,21 @@ function main() {
     }
     if (
       (p1Choice === 'rock' && p2Choice === 'scissors') ||
+      (p1Choice === 'rock' && p2Choice === 'lizard') ||
       (p1Choice === 'scissors' && p2Choice === 'paper') ||
-      (p1Choice === 'paper' && p2Choice === 'rock')
+      (p1Choice === 'scissors' && p2Choice === 'lizard') ||
+      (p1Choice === 'paper' && p2Choice === 'spock') ||
+      (p1Choice === 'paper' && p2Choice === 'rock') ||
+      (p1Choice === 'spock' && p2Choice === 'scissors') ||
+      (p1Choice === 'spock' && p2Choice === 'rock') ||
+      (p1Choice === 'lizard' && p2Choice === 'spock') ||
+      (p1Choice === 'lizard' && p2Choice === 'paper')
     ) {
       player1Wins++
       const player1WonGames = document.querySelector('section.player1 footer')
       player1WonGames.textContent = `Games Won: ${player1Wins}`
       return window.alert('Player 1 Wins!')
-    }
-    if (
-      (p2Choice === 'rock' && p1Choice === 'scissors') ||
-      (p2Choice === 'scissors' && p1Choice === 'paper') ||
-      (p2Choice === 'paper' && p1Choice === 'rock')
-    ) {
+    } else {
       player2Wins++
       const player2WonGames = document.querySelector('section.player2 footer')
       player2WonGames.textContent = `Games Won: ${player2Wins}`
